4a2babfc0e50e790cb7c2d30dfcc018b9ede3ca8
Martin Gulas <martingulas@Martins-MacBook-Air.local>
2026-05-06 17:44:41 +0200

    footer done

--- apps/matches/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Match
-
-
-@admin.register(Match)
-class MatchAdmin(admin.ModelAdmin):
-    list_display = ("club", "category", "opponent", "match_date", "competition", "is_published")
-    list_filter = ("club", "category", "competition", "is_published")
-    search_fields = ("opponent", "competition", "location")
--- apps/matches/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ClubMatchListView
-
-urlpatterns = [
-    path("<slug:club_slug>/", ClubMatchListView.as_view(), name="club-match-list"),
-]
